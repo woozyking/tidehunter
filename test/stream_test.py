6a560f8bd0c9af670126302afa0f19c9143f4e00
Leo Li <leo@addictivemobility.com>
2013-09-11 11:39:25 -0400

    added redis-server in travis config

--- test/stream_test.py
+++ test/stream_test.py
@@ -90,14 +90,16 @@ class HunterTestBasicAuth(unittest.TestCase):
 class HunterTestOAuth(HunterTest):
 
     def _conf(self):
+        oauth_config = {
+            'consumer_key': '5wgrdrBSvbfXBuopdGgY9Q',
+            'consumer_secret': 'GKtn1buVpvynFlDsFmTl8tRJmnbVsSmULRGUbEMNU',
+            'token_key': '974720028-xyfPogLX9ynU05xQYwYNiPtiZengdpdYbyxnpXBI',
+            'token_secret': 'rFDXV79FIG3881oxYT3XpYKEXi22bk74DaM7poYR0g'
+        }
+
         self.hunter_conf = {
             'url': 'https://stream.twitter.com/1.1/statuses/sample.json',
-            'oauth': {
-                'consumer_key': '5wgrdrBSvbfXBuopdGgY9Q',
-                'consumer_secret': 'GKtn1buVpvynFlDsFmTl8tRJmnbVsSmULRGUbEMNU',
-                'token_key': '974720028-xyfPogLX9ynU05xQYwYNiPtiZengdpdYbyxnpXBI',
-                'token_secret': 'rFDXV79FIG3881oxYT3XpYKEXi22bk74DaM7poYR0g'
-            },
+            'oauth': oauth_config,
             'limit': 5,
             'timeout': 30
         }
